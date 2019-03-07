4f3cb992b179cf9655c1f300bfb4507bf8818cee
xserrat <xavier.serrat22@gmail.com>
2019-03-07 21:33:20 +0100

    Store startup names selected when the favourite icon is pressed. Add a PageRoute where the stored names are shown.

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Namer',
+      theme: ThemeData.dark(),
       home: RandomWords(),
     );
   }
@@ -21,16 +21,45 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _wordPairFont = const TextStyle(fontSize: 18.0);
-  final _allSuggestions = <WordPair>[];
+  final Set<WordPair> _stored_words = new Set<WordPair>();
+  final List<WordPair> _allSuggestions = <WordPair>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Startup Namer'),
+        actions: <Widget>[
+          new IconButton(
+              icon: const Icon(Icons.list), onPressed: _showStoredWordsPage)
+        ],
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _showStoredWordsPage() {
+    Navigator.of(context).push(_buildStoredWordsRoute());
+  }
+
+  MaterialPageRoute _buildStoredWordsRoute() {
+    return MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> wordRows =
+          _stored_words.map((WordPair aWordPair) {
+        return ListTile(
+            title: Text(aWordPair.asPascalCase, style: _wordPairFont));
+      });
+
+      final List<Widget> wordRowsWithinEmptySpaces =
+          ListTile.divideTiles(tiles: wordRows, context: context).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Favourite Startup Names'),
+        ),
+        body: ListView(children: wordRowsWithinEmptySpaces),
+      );
+    });
   }
 
   Widget _buildSuggestions() {
@@ -61,11 +90,34 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   ListTile _buildRowWithWordPair(WordPair wordPair) {
+    final bool isAlreadySaved = _stored_words.contains(wordPair);
+
+    final IconData favouriteIconMarked = Icons.favorite;
+    final IconData favouriteIconUnmarked = Icons.favorite_border;
+
+    final Icon favouriteIcon = new Icon(
+        isAlreadySaved ? favouriteIconMarked : favouriteIconUnmarked,
+        color: isAlreadySaved ? Colors.red : null);
+    final IconButton favouriteButton = new IconButton(
+        icon: favouriteIcon,
+        onPressed: () {
+          setState(() {
+            if (isAlreadySaved) {
+              _stored_words.remove(wordPair);
+
+              return;
+            }
+
+            _stored_words.add(wordPair);
+          });
+        });
+
     return ListTile(
       title: Text(
         wordPair.asPascalCase,
         style: _wordPairFont,
       ),
+      trailing: favouriteButton,
     );
   }
 }
